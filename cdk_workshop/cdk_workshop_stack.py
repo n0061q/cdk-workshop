59d280d9089d53ce303ff55ee6829a55b3e95bc6
n0061q <95093640+n0061q@users.noreply.github.com>
2023-09-19 16:29:16 +0300

    apigw with lambda and hitcounter custom construct

--- cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop_stack.py
@@ -6,21 +6,24 @@ from aws_cdk import (
     aws_sqs as sqs,
     aws_sns as sns,
     aws_sns_subscriptions as subs,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
 )
+
+from .hitcounter import HitCounter
 
 
 class CdkWorkshopStack(Stack):
-
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "CdkWorkshopQueue",
-            visibility_timeout=Duration.seconds(300),
+        my_lambda = _lambda.Function(
+            self,
+            "HelloHandler",
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.from_asset("lambda"),
+            handler="hello.handler",
         )
+        hello_with_counter = HitCounter(self, "HelloHitCounter", downstream=my_lambda)
 
-        topic = sns.Topic(
-            self, "CdkWorkshopTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
+        apigw.LambdaRestApi(self, "Endpoint", handler=hello_with_counter.handler)
