d3329f6714f46b1edef728b41534deff8e72b5d9
n0061q <95093640+n0061q@users.noreply.github.com>
2023-09-21 16:58:58 +0300

    unit tests

--- tests/unit/test_cdk_workshop_stack.py
+++ tests/unit/test_cdk_workshop_stack.py
@@ -1,21 +1,71 @@
-import aws_cdk as core
-import aws_cdk.assertions as assertions
-from cdk_workshop.cdk_workshop_stack import CdkWorkshopStack
+import pytest
+from aws_cdk import Stack, aws_lambda as _lambda, assertions
+
+from cdk_workshop.hitcounter import HitCounter
 
 
-def test_sqs_queue_created():
-    app = core.App()
-    stack = CdkWorkshopStack(app, "cdk-workshop")
-    template = assertions.Template.from_stack(stack)
-
-    template.has_resource_properties("AWS::SQS::Queue", {
-        "VisibilityTimeout": 300
-    })
+@pytest.fixture
+def stack():
+    yield Stack()
 
 
-def test_sns_topic_created():
-    app = core.App()
-    stack = CdkWorkshopStack(app, "cdk-workshop")
-    template = assertions.Template.from_stack(stack)
+@pytest.fixture
+def downstream_lambda(stack):
+    yield _lambda.Function(
+        stack,
+        "TestFunction",
+        runtime=_lambda.Runtime.PYTHON_3_7,
+        handler="hello.handler",
+        code=_lambda.Code.from_asset("lambda"),
+    )
 
-    template.resource_count_is("AWS::SNS::Topic", 1)
+
+@pytest.fixture
+def hc(stack, downstream_lambda):
+    yield HitCounter(stack, "HitCounter", downstream=downstream_lambda)
+
+
+@pytest.fixture
+def hc_template(hc, stack):
+    yield assertions.Template.from_stack(stack)
+
+
+def test_hitcounter_ddb_table_created(hc_template):
+    hc_template.resource_count_is("AWS::DynamoDB::Table", 1)
+
+
+def test_hitcounter_lambda_has_env_vars(hc_template):
+    envCapture = assertions.Capture()
+
+    hc_template.has_resource_properties(
+        "AWS::Lambda::Function",
+        {
+            "Handler": "hitcount.handler",
+            "Environment": envCapture,
+        },
+    )
+
+    assert envCapture.as_object() == {
+        "Variables": {
+            "DOWNSTREAM_FUNCTION_NAME": {"Ref": "TestFunction22AD90FC"},
+            "HITS_TABLE_NAME": {"Ref": "HitCounterHits079767E5"},
+        },
+    }
+
+
+@pytest.mark.skip("TBD")
+def test_hitcounter_ddb_with_encryption(hc_template):
+    hc_template.has_resource_properties(
+        "AWS::DynamoDB::Table",
+        {
+            "SSESpecification": {
+                "SSEEnabled": True,
+            },
+        },
+    )
+
+
+@pytest.mark.parametrize("read_capacity", (1, 4, 21, 50, 100))
+def test_hitcounter_ddb_read_capacity_range(stack, downstream_lambda, read_capacity):
+    with pytest.raises(ValueError):
+        HitCounter(stack, "HitCounter", downstream=downstream_lambda, read_capacity=read_capacity)
